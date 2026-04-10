f99e687b26e30169d73a542353a3d84f2462efc2
mustapha04 <mustaphaelibrahimi6@gmail.com>
2026-04-10 17:34:17 -0400

    Save progress: 1980.1

--- student_info.sh
+++ student_info.sh
@@ -5,3 +5,5 @@
 echo -e "\n~~ My Computer Science Students ~~\n"
 
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
+
+echo -e "\nAll course names whose first letter is before 'D' in the alphabet:"
