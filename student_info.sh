6fa49e890456525d99c4415c1cff853c25a1acda
mustapha04 <mustaphaelibrahimi6@gmail.com>
2026-04-10 16:01:41 -0400

    Save progress: 1460.1

--- student_info.sh
+++ student_info.sh
@@ -12,3 +12,4 @@ echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE gpa = 4.0")
 echo -e "\nAll course names whose first letter is before 'D' in the alphabet:"
 echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE major_id IS NULL AND (first_name LIKE 'D%' OR gpa > 3.0)")"
 echo "$($PSQL "SELECT major_id, COUNT(*) AS number_of_students, ROUND(AVG(gpa), 2) AS average_gpa FROM students GROUP BY major_id HAVING COUNT(*) > 1 ORDER BY major_id")"
+echo "$($PSQL "SELECT major FROM students RIGHT JOIN majors ON students.major_id = majors.major_id WHERE student_id IS NULL OR first_name ILIKE '%ma%' ORDER BY major")"
