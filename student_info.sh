42ea01ba2c2e814ca40c42d5a7f317396a5fc690
mustapha04 <mustaphaelibrahimi6@gmail.com>
2026-04-10 18:20:50 -0400

    Save progress: 2430.1

--- student_info.sh
+++ student_info.sh
@@ -10,3 +10,4 @@ echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
 echo -e "\nAll course names whose first letter is before 'D' in the alphabet:"
 
 echo -e "\nLast name of students whose last name contains a case insensitive 'sa' or have an 'r' as the second to last letter:"
+echo "$($PSQL "SELECT last_name FROM students WHERE last_name ILIKE '%sa%' OR last_name LIKE '%r_'")"
