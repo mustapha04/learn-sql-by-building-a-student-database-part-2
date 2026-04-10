3fc60c615640b75107c6f747d21eb83490b6613d
mustapha04 <mustaphaelibrahimi6@gmail.com>
2026-04-10 17:29:20 -0400

    Save progress: 1860.1

--- student_info.sh
+++ student_info.sh
@@ -63,3 +63,4 @@ echo "$($PSQL "SELECT DISTINCT(course) FROM students FULL JOIN majors USING(majo
 echo -e "\nFirst name, last name, and GPA of students whose last name begins with an 'R' or after and have a GPA greater than 3.8 or less than 2.0:"
 echo "$($PSQL "SELECT last_name FROM students WHERE last_name ILIKE '%sa%' OR last_name LIKE '%r_'")"
 echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE major_id IS NULL AND (first_name LIKE 'D%' OR gpa > 3.0)")"
+echo "$($PSQL "SELECT course FROM courses WHERE course LIKE '_e%' OR course LIKE '%s' ORDER BY course DESC LIMIT 5")"
