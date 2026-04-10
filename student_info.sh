d10c57aeda7f1a5222c2384a92aefa293b8a3cbc
mustapha04 <mustaphaelibrahimi6@gmail.com>
2026-04-10 17:35:27 -0400

    Save progress: 2050.1

--- student_info.sh
+++ student_info.sh
@@ -7,3 +7,4 @@ echo -e "\n~~ My Computer Science Students ~~\n"
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 
 echo -e "\nAll course names whose first letter is before 'D' in the alphabet:"
+echo "$($PSQL "SELECT course FROM students INNER JOIN majors_courses USING(major_id) INNER JOIN courses USING(course_id) GROUP BY course HAVING COUNT(*) = 1 ORDER BY course")"
